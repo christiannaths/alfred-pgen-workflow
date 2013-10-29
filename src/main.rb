b2822831de8b19979ad32bf81ee1e59c74b241a3
Christian Naths <christiannaths@gmail.com>
2013-10-28 20:38:07 -0600

    initial commit

---
+++ src/main.rb
@@ -0,0 +1,13 @@
+require "rubygems"
+
+arg        = ARGV.first.nil? ? 4 : ARGV.first.to_i
+password   = []
+dictionary = File.expand_path(File.join(__FILE__, "..", "dictionary.txt"))
+words      = File.readlines(dictionary)
+
+arg.times do
+  segment = words.shuffle.slice!(0).strip
+  password.push(segment)
+end
+
+puts password.join('')
